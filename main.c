fcb3061cdf148796b9d13b17f4c6d11c5b23a291
BatmanHoussam <haitferouine@gmail.com>
2025-03-22 15:31:59 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int a, i, y;
 main(){
-	printf("entrez a = ");
+	printf("Enter a = ");
 	scanf("%d",&a);
-	printf("La table de multiplication de %d est :\n", a);
+	printf("The multiplication table of %d is :\n", a);
 	for (i = 0; i <= 12; i++){
 		y = a * i;
 		printf("%d x %d = %d \n", a, i, a * i);
